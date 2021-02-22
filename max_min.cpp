c9e4b1f83c12df6b6f0f58ac5709115ad5a47cdb
Vrinda Vyas <66427853+imvrinda@users.noreply.github.com>
2021-02-22 16:28:54 +0530

    Add files via upload
    Maximum and Minimum Number from Array

---
+++ max_min.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++){
+		cin>>a[i];
+	}
+	
+	int maxno=INT_MIN;
+	int minno=INT_MAX;
+	
+	for(int i=0;i<n;i++){
+		maxno=max(maxno,a[i]);
+		minno=min(minno,a[i]);
+	
+	}
+	cout<<maxno;
+	cout<<minno;
+	return 0;
+}
